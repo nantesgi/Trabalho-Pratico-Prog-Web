25b29b6dd8157628b2fda4015cb873d6e8a64f84
Raulleao <raulleaochagas@gmail.com>
2022-08-22 18:24:53 -0400

    Iniciando header

---
+++ front/src/components/HeaderComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="header">
+    <header id="container">
+      <div id="itens">
+        <div id="logo">LOJINHA</div>
+        <input type="text" name="barra de pesquisa" id="barraPesquisa" />
+        <button>+ Novo produto</button>
+        <img src="/trabalhoProgweb/sacola.png" alt="" />
+      </div>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+#container{
+    width: 100%;
+    height: 160px;
+    background-color: #8546F0;
+
+}
+
+#itens{
+    display: flex;
+    flex-direction: row;
+
+}
+
+#logo{
+
+}
+#barraPesquisa{
+    
+}
+</style>
