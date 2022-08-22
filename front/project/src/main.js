74fbb41e5cdf44daf931f8739d90dad0ddfb0b3d
Raulleao <raulleaochagas@gmail.com>
2022-08-22 17:27:18 -0400

    Instalando VueJS no projeto

---
+++ front/project/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+createApp(App).mount('#app')
